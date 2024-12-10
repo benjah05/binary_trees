bfb06b9819ce33e48dfd5aef5707f029fee812db
benjah05 <senzelebenjamine@gmail.com>
2024-12-10 00:49:31 -0800

    tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t rightHeight = 0, leftHeight = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 	leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
